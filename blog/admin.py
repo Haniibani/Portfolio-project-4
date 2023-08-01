f755c7192e1bc5ddf9d628b653f91a9b31c648a2
Haniibani <hanna.behmer@gmail.com>
2023-08-01 18:26:20 +0000

    Add Summernote to instructions field

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient
+from django_summernote.admin import SummernoteModelAdmin
+
 
 class IngredientInline(admin.TabularInline):
     model = Ingredient
     extra = 2
 
-class RecipeAdmin(admin.ModelAdmin):
+
+@admin.register(Recipe)
+class PostAdmin(SummernoteModelAdmin, admin.ModelAdmin):
+    summernote_fields = ('instructions',)
     inlines = (IngredientInline,)
 
-admin.site.register(Recipe, RecipeAdmin)
+
 admin.site.register(Ingredient)
