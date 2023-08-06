781ebb3a62469a1655c07856c5be35f69a85221d
Haniibani <hanna.behmer@gmail.com>
2023-08-06 14:08:29 +0000

    Add comments on admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -4,13 +4,24 @@ from django_summernote.admin import SummernoteModelAdmin
 
 
 class IngredientInline(admin.TabularInline):
+    """
+    Define an inline admin view for `Ingredient` model.
+    Allows managing ingredients directly within the `Recipe` form.
+    """
     model = Ingredient
     extra = 1
 
 
 @admin.register(Recipe)
 class PostAdmin(SummernoteModelAdmin, admin.ModelAdmin):
-
+    """
+    Admin view for the `Recipe` model. 
+    Integrates Summernote for rich text editing.
+    - Automatically populates 'slug' based on 'title'.
+    - Provides specific fields for display, filtering, and searching.
+    - Uses Summernote rich text editor for `instructions`.
+    - Includes inline view for ingredients.
+    """
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'tag')
     list_filter = ('status', 'created_on')
@@ -21,11 +32,18 @@ class PostAdmin(SummernoteModelAdmin, admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
+    """
+    Admin view for the `Comment` model.
+    - Specifies fields for display, filtering, and searching.
+    - Provides a bulk action to approve selected comments.
+    """
     list_display = ('name', 'body', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approved_comments']
 
     def approved_comments(self, request, queryset):
+        """
+        Action to bulk-approve selected comments.
+        """
         queryset.update(approved=True)
