34654230b91c32182d854533684e10e9fbdf81dd
Haniibani <hanna.behmer@gmail.com>
2023-09-05 17:30:49 +0000

    Set DEBUG to False

--- recipie/settings.py
+++ recipie/settings.py
@@ -29,7 +29,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-haniibani-portfolioproj-9xk1ej4mpmf.ws-eu104.gitpod.io",
